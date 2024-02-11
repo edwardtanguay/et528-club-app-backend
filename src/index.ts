505b9e23dbd51a4f9c59c4fc5c216fd7f0279fe3
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-12 00:16:21 +0100

    with error

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 app.use(cors());
@@ -8,16 +8,17 @@ const port = 3311;
 const members = [
 	{
 		id: "28jj21",
-		name: "Robert Hammerschmidt"
+		name: "Robert Hammerschmidt",
 	},
 	{
 		id: "ii34ja",
-		name: "Angela Wasserturm"
-	}
+		name: "Angela Wasserturm",
+	},
 ];
 
-app.get('/members', (req, res) => {
+app.get("/members", (req, res) => {
 	res.json(members);
+	//res.json("298jsfaskfasuh ... NETWORK ERROR 82734234: Missing characters in transition.");
 });
 
 app.listen(port, () => {
